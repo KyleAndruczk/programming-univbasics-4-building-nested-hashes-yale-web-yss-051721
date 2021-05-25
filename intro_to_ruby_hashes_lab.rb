1463c922cc33217cec83f8f02dcdc40cc8b0f499
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-25 17:50:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ monopoly = {}
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-  monopoly = { :railroads => {} }
+  monopoly = { :railroads=>{} }
 end
 
 def monopoly_with_second_tier
@@ -16,6 +16,39 @@ def monopoly_with_second_tier
   # monopoly[:railroads] << {:rent_in_dollars => {}}
   # monopoly[:railroads] << {:names => {}}
   
+  
+  monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+  }
+}
 end
 
 def monopoly_with_third_tier
