85da5e280a6fbd7663c6e949704aa06fcd3fd4cf
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-25 17:47:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
   # monopoly[:railroads][:names] = { :reading_railroad => {}, :pennsylvania => {}, 
   #   :b_and_o_railroad => {}, :shortline_railroad => {}
     
-  }
+  # }
 end
 
 def monopoly_with_fourth_tier
